7c5988ed1ab635369d9335e4905094f1a7c73891
jzg <jiao_zg22@163.com>
2018-03-16 15:17:21 +0800

    Linux 关闭zk

--- global-fairy-core/src/main/java/org/global/fairy/core/RunLinuxZKService.java
+++ global-fairy-core/src/main/java/org/global/fairy/core/RunLinuxZKService.java
@@ -5,35 +5,32 @@ import org.global.fairy.core.utils.ViewOSUtil;
 
 public class RunLinuxZKService implements RunZKService {
 	public Process ps = null;
-	public String path;
+	public String cmd;
 
 	@Override
 	public void runZKService() {
 		if (ViewOSUtil.isLinuxOS()) {
-			path = ZKEnvConfig.getZookeeperHomeEnv() + "/bin/zkServer.sh start";
+			cmd = ZKEnvConfig.getZookeeperHomeEnv() + "/bin/zkServer.sh start";
 		}else{
 			System.out.println("不是Linux系统");
 		}
-		System.out.println("zookeeper启动路径：" + path);
+		System.out.println("zookeeper启动路径：" + cmd);
 		// String path =
 		// "cmd.exe /c start d:/\"apache-zookeeper-3.4.8\"/bin\"/zkServer.cmd";
-		ps = CallExe.runCmd(path);
+		ps = CallExe.runCmd(cmd);
 	}
 
 	@Override
 	public void stopZKService() {
-		int i = ps.exitValue();
-		if (i == 0) {
-			System.out.println("ZKService stop 执行完成.");
-		} else {
-			System.out.println("ZKService stop 执行失败.");
+		if (ViewOSUtil.isLinuxOS()) {
+			cmd = ZKEnvConfig.getZookeeperHomeEnv() + "/bin/zkServer.sh stop";
+		}else{
+			System.out.println("不是Linux系统");
 		}
-		ps.destroy();
-		ps = null;
-
-		// 批处理执行完后，根据cmd.exe进程名称
-		// kill掉cmd窗口
-		killProcess();
+		System.out.println("zookeeper关闭命令：" + cmd);
+		// String path =
+		// "cmd.exe /c start d:/\"apache-zookeeper-3.4.8\"/bin\"/zkServer.cmd";
+		ps = CallExe.runCmd(cmd);
 		
 	}
 
