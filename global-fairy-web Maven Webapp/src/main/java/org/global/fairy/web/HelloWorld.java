b9b274ffb9ee32c7a1fdbf082bcc55ff7ece5a68
jzg <jiao_zg22@163.com>
2018-04-11 14:24:31 +0800

    提交readme英文支持

--- global-fairy-web Maven Webapp/src/main/java/org/global/fairy/web/HelloWorld.java
+++ global-fairy-web Maven Webapp/src/main/java/org/global/fairy/web/HelloWorld.java
@@ -3,13 +3,12 @@ package org.global.fairy.web;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.global.fairy.api.UserApi;
-import org.global.fairy.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping(value = "/helloWorld")
@@ -23,8 +22,7 @@ public class HelloWorld {
 	@ResponseBody()
 	public String sayHello(){
 		logger.info("PRINT A MARK IN THE METHOD");
-		userApi.sayHello();
-		
+		userApi.sayHello("ddd");
 		return "HelloWorld";
 	}
 	
@@ -32,7 +30,7 @@ public class HelloWorld {
 	@ResponseBody()
 	public String registor(){
 		logger.info("PRINT A MARK IN THE METHOD");
-		userApi.sayHello();
+		userApi.sayHello("aaa");
 		return "HelloWorld";
 	}
 }
