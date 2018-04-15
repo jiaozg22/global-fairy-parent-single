fb105751abf4a619d9d8748ef0163a4e20499c71
jzg <jiao_zg22@163.com>
2018-04-15 15:04:31 +0800

    通用service

--- global-fairy-service-impl/src/main/java/org/global/fairy/service/impl/CommonServiceImpl.java
+++ global-fairy-service-impl/src/main/java/org/global/fairy/service/impl/CommonServiceImpl.java
@@ -3,7 +3,13 @@ package org.global.fairy.service.impl;
 import org.global.fairy.service.ICommonService;
 import org.springframework.stereotype.Service;
 
+/**
+ * 通用service
+ * 
+ * @author jiao
+ * 
+ */
 @Service
 public class CommonServiceImpl implements ICommonService {
-	
+
 }
