d983727a40bbcbacfbe3311cec22921831ffc069
jzg <jiao_zg22@163.com>
2018-05-13 17:10:27 +0800

    common代码暂时删除

--- global-fairy-service-impl/src/main/java/org/global/fairy/service/impl/CommonServiceImpl.java
+++ global-fairy-service-impl/src/main/java/org/global/fairy/service/impl/CommonServiceImpl.java
@@ -1,15 +1,24 @@
-package org.global.fairy.service.impl;
-
-import org.global.fairy.service.ICommonService;
-import org.springframework.stereotype.Service;
-
-/**
- * 通用service
- * 
- * @author jiao
- * 
- */
-@Service
-public class CommonServiceImpl implements ICommonService {
-
-}
+//package org.global.fairy.service.impl;
+//
+//import org.global.fairy.dao.CommonDao;
+//import org.global.fairy.service.ICommonService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * 通用service
+// * 
+// * @author jiao
+// * 
+// */
+//@Service
+//public class CommonServiceImpl implements ICommonService {
+//	
+//	@Autowired
+//	CommonDao commonDao ;
+//	
+//	public <T> int add(T po) {
+//		return commonDao.add(po);
+//	}
+//
+//}
--- global-fairy-service-impl/src/main/java/org/global/fairy/service/impl/UserServiceImpl.java
+++ global-fairy-service-impl/src/main/java/org/global/fairy/service/impl/UserServiceImpl.java
@@ -18,8 +18,6 @@ public class UserServiceImpl implements IUserService{
 	@Override
 	public void sayHello(User user) {
 		
-		
-		
 		logger.info("into service");
 		
 		userMapper.insertUser(user);
